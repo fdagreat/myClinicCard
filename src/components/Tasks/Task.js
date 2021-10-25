1691efe019fe8c970ae76527c4e4bc2e64aa492e
kitindi <abdullkitindi@gmail.com>
2021-10-25 12:50:52 +0300

    added activities list component

--- src/components/Tasks/Task.js
+++ src/components/Tasks/Task.js
@@ -5,7 +5,12 @@ const Task = ({ task }) => {
   console.log(task);
   return (
     <div className={classes.taskbox}>
-      <div className="time">{task.time}</div>
+      <div
+        className="time"
+        style={{ paddingRight: ".5rem", borderRight: "1px solid #4B5563" }}
+      >
+        {task.time}
+      </div>
       <div className="name">{task.name}</div>
       <div className="checkbox">
         <input type="checkbox" />
