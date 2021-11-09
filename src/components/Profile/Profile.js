d619c3779e51d130746d94fe2c412ce87092909a
kitindi <abdullkitindi@gmail.com>
2021-11-09 22:51:00 +0300

    added layout to the profile and learn pages

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,7 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import classes from "./Profile.module.css";
+import { Modal } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+// import { useHistory } from "react-router";
+import { IoLogOutOutline } from "react-icons/io5";
+import ActivityForm from "../Form/ActivityForm";
+
+import Menu from "../Menu/Menu";
 
 const Profile = () => {
-  return <div>Edit profile</div>;
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => {
+    setShow(false);
+  };
+
+  return (
+    <>
+      <div className={classes.grid}>
+        {/* menu column */}
+        <div className={classes.menu}>
+          <Link to="/" className={classes.topbar}>
+            <span>
+              <IoLogOutOutline className={classes.icon} />
+            </span>
+            Logout
+          </Link>
+          {/* menu item lists */}
+          <Menu />
+        </div>
+        {/* content column */}
+        <div className={classes.content}>
+          <div className={classes.main}></div>
+        </div>
+      </div>
+      {/* modal component */}
+      <div>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title
+              style={{
+                fontSize: "1.05em",
+                fontWeight: "600",
+                color: "#34d399",
+              }}
+            >
+              Add New Activity
+            </Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <ActivityForm handleClose={handleClose} />
+          </Modal.Body>
+          {/* <Modal.Footer>
+            <Button
+              variant="primary"
+              onClick={handleClose}
+              className={classes.btn}
+              style={{ borderRadius: "0px" }}
+              type="submit"
+            >
+              Save
+            </Button>
+          </Modal.Footer> */}
+        </Modal>
+      </div>
+    </>
+  );
 };
 
 export default Profile;
