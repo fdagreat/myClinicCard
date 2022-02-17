0ee20f667ca7e9f8f23e846d4878cc68eccb2cd7
kitindi <abdullkitindi@gmail.com>
2022-02-17 11:21:55 +0300

    added the report component

--- src/components/Reports/ClinicReport.js
+++ src/components/Reports/ClinicReport.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 import classes from "./ClinicReport.module.css";
 import { Modal } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
+import profileImage from "../../assets/avartar.jpg";
 // import { useHistory } from "react-router";
 import { IoLogOutOutline } from "react-icons/io5";
 import ActivityForm from "../Form/ActivityForm";
@@ -32,39 +33,36 @@ const ClinicReport = () => {
         </div>
         {/* content column */}
         <div className={classes.content}>
-          <div className={classes.main}></div>
+          <div className={classes.main}>
+            {/* top bar section */}
+            <div className={classes.top_bar}>
+              <div className={classes.title}>Clinic Progress Reports</div>
+              <div className={classes.avarta}>
+                <div className={classes.avarta_btn}>
+                  <button>
+                    <span className={classes.plus_icon}>+</span> Create New
+                    Report
+                  </button>
+                </div>
+                <div className={classes.user_data}>
+                  <div className={classes.avarta_image}>
+                    <img src={profileImage} alt="" />
+                  </div>
+                  <span>Jane Doe</span>
+                </div>
+              </div>
+            </div>
+            {/* cards section */}
+            <section className={classes.card_grid}>
+              <div className="card_item">1</div>
+              <div className="card_item">2</div>
+              <div className="card_item">3</div>
+              <div className="card_item">4</div>
+            </section>
+          </div>
         </div>
       </div>
       {/* modal component */}
-      <div>
-        <Modal show={show} onHide={handleClose}>
-          <Modal.Header closeButton>
-            <Modal.Title
-              style={{
-                fontSize: "1.05em",
-                fontWeight: "600",
-                color: "#34d399",
-              }}
-            >
-              Add New Activity
-            </Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <ActivityForm handleClose={handleClose} />
-          </Modal.Body>
-          {/* <Modal.Footer>
-            <Button
-              variant="primary"
-              onClick={handleClose}
-              className={classes.btn}
-              style={{ borderRadius: "0px" }}
-              type="submit"
-            >
-              Save
-            </Button>
-          </Modal.Footer> */}
-        </Modal>
-      </div>
     </>
   );
 };
